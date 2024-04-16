4d9eadc99ab69d776a60e850d5942cc0e14d6183
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-04-16 13:53:19 +0700

    add put image

--- src/query/image.ts
+++ src/query/image.ts
@@ -1,9 +1,17 @@
 import { Hono } from "hono";
 import { env } from "hono/adapter";
+import { detectType } from "../utils";
+import { sha256 } from "hono/utils/crypto";
 
 type Bindings = {
   DB: D1Database;
   MY_BUCKET: R2Bucket;
+};
+
+type Data = {
+  body: string;
+  width?: string;
+  height?: string;
 };
 
 const images = new Hono<{ Bindings: Bindings }>();
@@ -19,6 +27,32 @@ images.post("/upload", async (c) => {
   } catch (e) {
     return c.json({ err: e }, 500);
   }
+});
+
+images.put("/upload", async (c) => {
+  const data = await c.req.json<Data>();
+  const base64 = data.body;
+  if (!base64) return c.notFound();
+
+  const type = detectType(base64);
+  const body = Uint8Array.from(atob(base64), (c) => c.charCodeAt(0));
+
+  let key;
+  if (data.width && data.height) {
+    key =
+      (await sha256(body)) +
+      `_${data.width}x${data.height}` +
+      "." +
+      type?.suffix;
+  } else {
+    key = (await sha256(body)) + "." + type?.suffix;
+  }
+
+  await c.env.MY_BUCKET.put(key, body, {
+    httpMetadata: { contentType: type?.mimeType },
+  });
+
+  return c.text(key);
 });
 
 images.get("/:key", (c) => {
